fcc80aaa6dc8f03c73352caef317f83d4b75a49f
James H. Nguyen <james.nguyen@flyr.com>
2026-07-03 11:51:08 -0700

    fix(0-bootstrap): grant billing.admin to billing admins group

--- 0-bootstrap/index.ts
+++ 0-bootstrap/index.ts
@@ -167,6 +167,12 @@ export = async () => {
         });
     }
 
+    new gcp.organizations.IAMMember(`org-billing-admins-admin`, {
+        orgId: cfg.orgId,
+        role: "roles/billing.admin",
+        member: `group:${cfg.groups.requiredGroups.groupBillingAdmins}`,
+    });
+
     /*************************************************
       Outputs — mirrors outputs.tf and outputs_cb.tf
     *************************************************/
